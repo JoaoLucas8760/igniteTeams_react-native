f4ed12245031a6ad83d951aba7db6f62cf038393
perfluka8760 <canalluka8760@gmail.com>
2023-03-29 13:32:53 -0300

    initial commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@components/Header";
+import { StyleSheet, Text, View } from "react-native";
+import { Container } from "./styles";
+
+export function Groups() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
