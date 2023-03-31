7fbc0a3b4818e86940109e34c9bee8ca6671ca1a
perfluka8760 <canalluka8760@gmail.com>
2023-03-31 14:52:50 -0300

    Group added

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,27 @@
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { StyleSheet, Text, View } from "react-native";
+import ListEmpty from "@components/LIstEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
