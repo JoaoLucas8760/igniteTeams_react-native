d7bb139b19088c33a17d349312aea99fe21bd8bc
perfluka8760 <canalluka8760@gmail.com>
2023-04-01 16:58:56 -0300

    Create component Filter Teams

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Input } from "@components/Input";
+import { FlatList } from "react-native";
+import { Container, Form } from "./styles";
 
 export default function Players() {
+  const [team, setTeam] = useState("Time A");
   return (
     <Container>
       <Header showBackButton />
       <Highlight
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <FlatList
+        data={["Time A", "Time B"]}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Filter
+            title={item}
+            isActive={item === team}
+            onPress={() => setTeam(item)}
+          />
+        )}
+        horizontal
       />
     </Container>
   );
