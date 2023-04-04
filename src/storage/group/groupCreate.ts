aea1cc0d78c8f8fae8521215bdde95bfda25ea79
perfluka8760 <canalluka8760@gmail.com>
2023-04-03 21:05:57 -0300

    Function getStorage

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUPS_COLLECTION } from "@storage/storageConfig";
-import { err } from "react-native-svg/lib/typescript/xml";
+import { groupsGetAll } from "./groupsGetAll";
 
 export async function GroupCreate(newGroup: string) {
   try {
-    await AsyncStorage.setItem(GROUPS_COLLECTION, newGroup);
+    const storedGroups = await groupsGetAll();
+
+    const storage = JSON.stringify([...storedGroups, newGroup]);
+    await AsyncStorage.setItem(GROUPS_COLLECTION, storage);
   } catch (error) {
     throw error;
   }
---
+++ src/storage/group/groupsGetAll.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GROUPS_COLLECTION } from "@storage/storageConfig";
+
+export async function groupsGetAll() {
+  try {
+    const storage = await AsyncStorage.getItem(GROUPS_COLLECTION);
+
+    const groups: string[] = storage ? JSON.parse(storage) : [];
+
+    return groups;
+  } catch (error) {
+    throw error;
+  }
+}
