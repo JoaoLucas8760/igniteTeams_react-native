1a259cd452d3cdfc2fb04fb7e4cf5044e7414f2f
perfluka8760 <canalluka8760@gmail.com>
2023-04-03 21:31:09 -0300

    Create a util/AppError

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,11 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUPS_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function GroupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
 
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError("Já Existe um grupo cadastrado com esse nome.");
+    }
     const storage = JSON.stringify([...storedGroups, newGroup]);
     await AsyncStorage.setItem(GROUPS_COLLECTION, storage);
   } catch (error) {
