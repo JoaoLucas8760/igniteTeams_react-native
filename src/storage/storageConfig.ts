befa179d27ced2a82b62ce2b4323ee9b6299e0da
perfluka8760 <canalluka8760@gmail.com>
2023-04-05 13:44:39 -0300

    players storage

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUPS_COLLECTION = "@ignite-teams:groups";
+const PLAYER_COLLECTION = "@ignite-teams:players";
 
-export { GROUPS_COLLECTION };
+export { GROUPS_COLLECTION, PLAYER_COLLECTION };
